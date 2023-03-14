a940d22aa128e969424b52208891480923980bf9
Olivier Nolibois <olivier.nolibois@avanade.com>
2023-03-14 15:31:55 +0100

    Home, system administration, unsent email OK

--- MobileMonitoring/Shared/EmailDto.cs
+++ MobileMonitoring/Shared/EmailDto.cs
@@ -2,6 +2,10 @@
 {
     public record EmailDto(int IdEmail, string UserSender, string UserReceiver, string Subject, DateTime CreationDate)
     {
+        #region for deserialyzation only
+        public EmailDto() : this(default, "", "", "", default) { }
+        #endregion
+
         public EmailDto(Email email) : this(email.IdEmail, email.UserSender.FullName, email.UserReceiver.FullName, email.Subject, email.CreationDate) { }
     }
 }
--- MobileMonitoring/Shared/UserDto.cs
+++ MobileMonitoring/Shared/UserDto.cs
@@ -2,6 +2,10 @@
 {
     public record UserDto(int IdUser, string User, int DynamicsId, string Company)
     {
+        #region for deserialyzation only
+        public UserDto() : this(default, "", default, "") { }
+        #endregion
+
         public UserDto(User user) : this(user.IdUser, user.FullName, user.DynamicsId, user.Company.Name) { }
     }
 }
